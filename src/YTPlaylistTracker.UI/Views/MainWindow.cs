7ebd1903fed388417e1fa91b321ac23c4ce3f411
Andrii S <7killua@gmail.com>
2026-04-07 23:24:10 +0300

    style(ui): add second hint bar with F-key and extended bindings

--- src/YTPlaylistTracker.UI/Views/MainWindow.cs
+++ src/YTPlaylistTracker.UI/Views/MainWindow.cs
@@ -55,7 +55,7 @@ public partial class MainWindow(
         {
             X = 0, Y = 0,
             Width = 18,
-            Height = Dim.Fill(1)
+            Height = Dim.Fill(2)
         };
         _profileList = new ListView
         {
@@ -70,7 +70,7 @@ public partial class MainWindow(
             X = Pos.Right(profileFrame),
             Y = 0,
             Width = 28,
-            Height = Dim.Fill(1)
+            Height = Dim.Fill(2)
         };
         _playlistList = new ListView
         {
@@ -85,7 +85,7 @@ public partial class MainWindow(
             X = Pos.Right(playlistFrame),
             Y = 0,
             Width = Dim.Fill(),
-            Height = Dim.Fill(1)
+            Height = Dim.Fill(2)
         };
         _videoTable = new TableView
         {
@@ -112,13 +112,19 @@ public partial class MainWindow(
 
         Add(profileFrame, playlistFrame, _videoFrame);
 
-        var hintBar = new Label(" h/l:pane  j/k:nav  Enter:detail  /:search  o:sort  e:export  H:history  ?:help  a:add  t:track  s:sync  S:all  q:quit")
+        var hintBar1 = new Label(" h/l:pane  j/k:nav  J/K:fast  Enter:detail  /:search  o:sort  a:add  t:track  T:all  s:sync  S:all  q:quit")
+        {
+            Y = Pos.AnchorEnd(2),
+            Width = Dim.Fill(),
+            ColorScheme = Colors.Menu
+        };
+        var hintBar2 = new Label(" F1:add  F2:track  F5:sync  F6:sync all  F8:deleted  F9:settings  e:export  H:history  u:update  ?:help")
         {
             Y = Pos.AnchorEnd(1),
             Width = Dim.Fill(),
             ColorScheme = Colors.Menu
         };
-        Add(hintBar);
+        Add(hintBar1, hintBar2);
     }
 
     public async Task InitializeAsync()
